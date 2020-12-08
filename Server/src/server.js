0f0ba93106c4a0dfa308258f226964fd9525d58c
Lars Johan Nybø <lars.johan.nybo@headit.no>
2020-12-08 10:03:00 +0100

    feat:server: Added basic API

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,8 +1,35 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.send("Welcome to the PWA-Demo API");
+const FENCES = [];
+
+function Point(lat, lon) {
+  this.lat = lat;
+  this.lon = lon;
+}
+
+function Fence(points = []) {
+  this.id = FENCES.length;
+  this.points = points;
+}
+
+
+app.get('/', (req, res) => {
+  return res.send('Welcome to the PWA-Demo API');
 });
+
+app.get('/fences', (req, res) => {
+  return res.status(200).contentType('application/json').send(FENCES);
+});
+
+app.post('/fence', (req, res) => {
+  const points = req.body;
+  if (!points || !Array.isArray(points) || points.length === 0) {
+    return res.status(400).send('body must be array of points: [{lat: \'latitude\', lon: \'longitude\'}]');
+  }
+  const fence = new Fence(points.map(p => new Point(p.lat, p.lon)));
+  FENCES.push(fence);
+  return res.status(200).contentType('application/json').send(fence);
+})
 
 app.listen(process.env.PORT || 8080);
